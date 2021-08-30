2015144ed4838568a55a0fd31530c240b558e29e
emykumba <emy@meetkumba.com>
2021-08-30 03:10:55 +0100

    addes side pannel

--- dummy_data/Heading.widget.tsx
+++ dummy_data/Heading.widget.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
+import Toolbar from '../src/components/Toolbar/Toolbar';
 import { JumbotronType } from '../src/components/Widgets/Jumbotron/Jombotron.type';
 
 interface Props {
@@ -6,15 +7,29 @@ interface Props {
 }
 
 export default function HeadingWidget({}: Props): ReactElement {
+  const [showTools, setShowTools] = useState<boolean>(false);
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="page-title-box p-3">
-              <h3 className="page-title">Dashboard</h3>
+      <>
+        <div className="container" onClick={() => setShowTools(!showTools)}>
+          <div className="row">
+            <div className="col-12">
+              <div className="page-title-box p-3">
+                <h3 className="page-title">Dashboard</h3>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        {showTools ? (
+          <Toolbar>
+            <h2 className="text-white">Heading</h2>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              corrupti doloribus inventore nisi quae sapiente quisquam cum
+              perspiciatis autem nesciunt vero error! Veritatis nihil aliquid
+              recusandae commodi repellat nulla ex.
+            </p>
+          </Toolbar>
+        ) : null}
+      </>
     );
 }
--- dummy_data/Jumbotron.widget.tsx
+++ dummy_data/Jumbotron.widget.tsx
@@ -1,26 +1,38 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import Toolbar from "../src/components/Toolbar/Toolbar";
 import { JumbotronType } from "../src/components/Widgets/Jumbotron/Jombotron.type";
 
 interface Props {
-    widget_data: JumbotronType;
+  widget_data: JumbotronType;
 }
 
-export default function JumbotronWidget({widget_data}: Props): ReactElement {
+export default function JumbotronWidget({ widget_data }: Props): ReactElement {
+  const [showTools, setShowTools] = useState<boolean>(false);
   return (
-    <div className="p-5 mb-4 bg-light rounded-3">
-      <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">{widget_data.heading}</h1>
-        <p className="col-md-8 fs-4">
-          {widget_data.sub_heading}
-        </p>
-        <button className="btn btn-primary btn-lg" type="button">
-          Example button
-        </button>
+    <>
+      <div
+        className="p-5 mb-4 bg-light rounded-3"
+        onClick={() => setShowTools(!showTools)}
+      >
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">{widget_data.heading}</h1>
+          <p className="col-md-8 fs-4">{widget_data.sub_heading}</p>
+          <button className="btn btn-primary btn-lg" type="button">
+            Example button
+          </button>
+        </div>
       </div>
-      <Toolbar>
-        <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, corrupti doloribus inventore nisi quae sapiente quisquam cum perspiciatis autem nesciunt vero error! Veritatis nihil aliquid recusandae commodi repellat nulla ex.</p>
-      </Toolbar>
-    </div>
+      {showTools ? (
+        <Toolbar>
+          <h2 className="text-white">Jumbotron</h2>
+          <p className="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+            corrupti doloribus inventore nisi quae sapiente quisquam cum
+            perspiciatis autem nesciunt vero error! Veritatis nihil aliquid
+            recusandae commodi repellat nulla ex.
+          </p>
+        </Toolbar>
+      ) : null}
+    </>
   );
 }
